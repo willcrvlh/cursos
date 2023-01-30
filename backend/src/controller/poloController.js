eca43cb76fd55d812b21862267b0fdca05cfb1fa
Willian Carvalho da Silva <wcarvalho91610@gmail.com>
2023-01-30 12:19:35 -0300

    criei a rota get e post

--- backend/src/controller/poloController.js
+++ backend/src/controller/poloController.js
@@ -1,9 +1,46 @@
+import poloModel from "../model/poloModel.js" 
+
 const poloController = app =>{
-    app.get('/polo', (req,res)=>{
-        res.send("get")
+    app.get('/polo', async (req,res)=>{
+        try {
+            const polo = await poloModel.findAll();
+            res.status(200).json(polo)
+        } catch (error) {
+            res.status(400).json({
+                "error": error
+            })
+        }
     })
-    app.post('/polo', (req,res)=>{
-        res.send("post")})
+    app.get('/polo/:id', async (req,res)=>{
+        const id = req.params.id
+        if (!poloModel) {
+            return res.status(422).json({ 
+                "error" : "polo não encontrado"
+            })
+        }
+        try{
+            const polo = await poloModel.find({_id: id})
+            res.status(200).json({polo})
+        }catch(e){
+            res.status(400).json({
+                "erro" : e
+            })
+        }
+    })
+
+    app.post('/polo',async (req,res)=>{
+        await poloModel.create(req.body)
+        try {
+            return res.status(201).json({
+                error: false,
+                "msg": "Polo criado com sucesso"
+            })
+        } catch (error) {
+            return res.status(400).json({
+                "error": error
+            })
+        }
+    })
 }
 
 export default poloController
